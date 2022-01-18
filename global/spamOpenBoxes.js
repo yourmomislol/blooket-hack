088f6ef6a22bddc8a319fe617389e92e863a5a9e
gliz <73669084+glixzzy@users.noreply.github.com>
2022-01-17 20:31:33 -0600

    change token to session

--- global/spamOpenBoxes.js
+++ global/spamOpenBoxes.js
@@ -4,7 +4,7 @@ const sleep = (ms) => new Promise(r => setTimeout(r, ms));
     const box = prompt('What box do you want to open? (EXAMPLE: Space)');
     const amount = prompt('How many boxes do you want to open?');
 
-    const response = await fetch('https://api.blooket.com/api/users/verify-token', { credentials: "include" });
+    const response = await fetch('https://api.blooket.com/api/users/verify-session', { credentials: "include" });
     const data = await response.json();
 
 
